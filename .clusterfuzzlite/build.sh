fcaf5684739319fd890b3f1241f5b9ab598ed5ae
Prince Amoako Atta <princeamoakoatta22@gmail.com>
2026-06-19 23:20:07 +0000

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -eu
 
-# Install internal dependencies inside the compilation image
-pip3 install atheris
-
 # Compile and bundle the Atheris harness into a self-contained executable within $OUT
 compile_python_fuzzer fuzz/target_fuzzer.py
