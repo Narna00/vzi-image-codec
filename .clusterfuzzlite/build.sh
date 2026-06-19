432b4ad10c358ae5825db19908c5d3896e40dd3b
Prince Amoako Atta <princeamoakoatta22@gmail.com>
2026-06-19 23:39:20 +0000

    Remove unnecessary comment in build.sh
    Remove comment about passing --paths to PyInstaller.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,5 +4,4 @@
 pip3 install atheris
 
 # 2. Compile the harness using the official wrapper tool
-# We pass --paths=. so PyInstaller can find your 'src' directory from the root
 compile_python_fuzzer fuzz/target_fuzzer.py --paths=.
