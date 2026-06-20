9dfa079b183abd376a20b38400539d26574b8fa0
Prince Amoako Atta <princeamoakoatta22@gmail.com>
2026-06-20 15:25:32 +0000

    Update parser.c

--- myparser/parser.c
+++ myparser/parser.c
@@ -1,6 +1,3 @@
-#include <Python.h>
-#include <string.h>
-
 static PyObject* parse(PyObject* self, PyObject* args) {
     PyObject* data_obj;
     if (!PyArg_ParseTuple(args, "O", &data_obj))
@@ -12,6 +9,13 @@ static PyObject* parse(PyObject* self, PyObject* args) {
     const char* data = PyBytes_AsString(data_obj);
     Py_ssize_t size = PyBytes_Size(data_obj);
 
+    // ---- ADD THIS BOUNDS CHECK ----
+    if (size > 64) {
+        PyErr_SetString(PyExc_ValueError, "Input size exceeds buffer capacity");
+        return NULL;
+    }
+    // ------------------------------
+
     // Vulnerable fixed-size buffer
     char buffer[64];
 
@@ -21,21 +25,4 @@ static PyObject* parse(PyObject* self, PyObject* args) {
     // Return a bytes object to prevent optimization
     Py_ssize_t ret_size = size < 64 ? size : 64;
     return PyBytes_FromStringAndSize(buffer, ret_size);
-}
-
-static PyMethodDef methods[] = {
-    {"parse", parse, METH_VARARGS, "Parse input data"},
-    {NULL, NULL, 0, NULL}
-};
-
-static struct PyModuleDef module = {
-    PyModuleDef_HEAD_INIT,
-    "parser",
-    NULL,
-    -1,
-    methods
-};
-
-PyMODINIT_FUNC PyInit_parser(void) {
-    return PyModule_Create(&module);
 }
